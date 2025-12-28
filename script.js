33e2fa4c8e4257a13f2bdfda3132858415dc0700
Hirishikesh <hirishikeshg25ml@srishakthi.ac.in>
2025-12-28 15:35:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let total = 0;
+
+function addToCart(name, price) {
+    const cart = document.getElementById("cart");
+
+    const li = document.createElement("li");
+    li.textContent = `${name} - ₹${price}`;
+    cart.appendChild(li);
+
+    total += price;
+    document.getElementById("total").textContent = total;
+}
